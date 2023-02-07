9b79286beb403612380f1d249482e78800a1b6ea
Floppa <rschuhler@hotmail.fr>
2023-02-07 03:17:41 +0100

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  moz_overlay = import (builtins.fetchTarball 
+    https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+
+  pkgs.mkShell {
+    buildInputs = [
+      nixpkgs.latest.rustChannels.stable.rust
+    ];
+  }
